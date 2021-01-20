d37c622cbbc200c560f182ce5e4dfd3ee62a7b12
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-01-20 19:59:14 -0300

    Definindo as entidades

---
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Cliente : Entity, IAggregateRoot
+    {
+        public Cliente(string nome, string email, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+
+        //Ef
+        protected Cliente() { }
+
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+    }
+}
