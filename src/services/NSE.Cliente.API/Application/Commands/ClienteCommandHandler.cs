ffe03bc70a079228ae5975720591cc6102d67205
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-01-21 08:21:15 -0300

    Command e Command Handler

---
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        public void Manipular(RegistrarClienteCommand message)
+        {
+            //validar cmd
+            //persistir na base
+        }
+    }
+}
---
+++ src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -0,0 +1,26 @@
+﻿using NSE.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Application.Commands
+{
+    public class RegistrarClienteCommand : Command
+    {
+        public RegistrarClienteCommand(Guid id, string nome, string email, string cpf)
+        {
+            AggregateId = Id;
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+    }
+}
