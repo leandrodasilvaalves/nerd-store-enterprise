597c346d89fc097825913ae9f4ab1cac1f817a36
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-01-30 14:14:23 -0300

    Implementando o carrinho no front-end - pt III

---
+++ src/web/NSE.WebApp.MVC/Extensions/CarrinhoViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        private readonly ICarrinhoService _carrinhoService;
+
+        public CarrinhoViewComponent(ICarrinhoService carrinhoService)
+        {
+            _carrinhoService = carrinhoService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _carrinhoService.ObterCarrinho() ?? new CarrinhoViewModel());
+        }
+    }
+}
