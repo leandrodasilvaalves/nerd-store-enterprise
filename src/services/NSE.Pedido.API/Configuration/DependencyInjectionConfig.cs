7661375d6349b6688549624481e6b908b757a47d
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-01-31 12:33:07 -0300

    Aplicando o voucher via BFF Compras

--- src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using NSE.Core.Mediator;
+using NSE.Pedidos.API.Application.Queries;
+using NSE.Pedidos.Domain.Vouchers;
+using NSE.Pedidos.Infra.Data;
+using NSE.Pedidos.Infra.Data.Repository;
+using NSE.WebAPI.Core.Usuario;
 
 namespace NSE.Pedidos.API.Configuration
 {
@@ -8,8 +14,8 @@ namespace NSE.Pedidos.API.Configuration
         public static void RegisterServices(this IServiceCollection services)
         {
             //// API
-            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            //services.AddScoped<IAspNetUser, AspNetUser>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
 
             //// Commands
             //services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
@@ -19,13 +25,13 @@ namespace NSE.Pedidos.API.Configuration
 
             //// Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
-            //services.AddScoped<IVoucherQueries, VoucherQueries>();
+            services.AddScoped<IVoucherQueries, VoucherQueries>();
             //services.AddScoped<IPedidoQueries, PedidoQueries>();
 
             //// Data
             //services.AddScoped<IPedidoRepository, PedidoRepository>();
-            //services.AddScoped<IVoucherRepository, VoucherRepository>();
-            //services.AddScoped<PedidosContext>();
+            services.AddScoped<IVoucherRepository, VoucherRepository>();
+            services.AddScoped<PedidosContext>();
         }
     }
 }
