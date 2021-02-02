5eab8964021919cc97db69fe36c27f2f4384ae16
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-02-02 20:29:54 -0300

    Camada de application de pedido - pt II

--- src/services/NSE.Pedidos.Domain/Vouchers/IVoucherRepository.cs
+++ src/services/NSE.Pedidos.Domain/Vouchers/IVoucherRepository.cs
@@ -6,5 +6,7 @@ namespace NSE.Pedidos.Domain.Vouchers
     public interface IVoucherRepository : IRepository<Voucher>
     {
         Task<Voucher> ObterVoucherPorCodigo(string codigo);
+
+        void Atualizar(Voucher voucher);
     }
 }
