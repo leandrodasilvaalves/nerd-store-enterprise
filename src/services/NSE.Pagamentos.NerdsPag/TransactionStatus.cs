c139a86508f7f690aae3f1fc088ad1eb22f62c86
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-02-08 20:34:02 -0300

    Entendendo o Gateway de pagamento

---
+++ src/services/NSE.Pagamentos.NerdsPag/TransactionStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.Pagamentos.NerdsPag
+{
+    public enum TransactionStatus
+    {
+        Authorized = 1,
+        Paid,
+        Refused,
+        Chargedback,
+        Cancelled
+    }
+}
