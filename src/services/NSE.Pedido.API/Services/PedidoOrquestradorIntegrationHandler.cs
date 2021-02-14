17cae03a890bffb1315dae9806a8cf4d38e52d22
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-02-14 12:01:12 -0300

    Rodando tarefas agendadas de forma nativa

---
+++ src/services/NSE.Pedido.API/Services/PedidoOrquestradorIntegrationHandler.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.Pedidos.API.Services
+{
+    public class PedidoOrquestradorIntegrationHandler : IHostedService, IDisposable
+    {
+        private readonly ILogger<PedidoOrquestradorIntegrationHandler> _logger;
+        private Timer _timer;
+
+        public PedidoOrquestradorIntegrationHandler(ILogger<PedidoOrquestradorIntegrationHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Servico de pedidos iniciado");
+            _timer = new Timer(ProcessarPedidos, null, TimeSpan.Zero, TimeSpan.FromSeconds(15));
+            return Task.CompletedTask;
+        }
+        
+        private async void ProcessarPedidos(object state)
+        {
+            _logger.LogInformation("Processando pedidos");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Servico de pedidos finalizado");
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
