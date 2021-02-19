dbd151d9a6b6d39a16079a6f1835b71fdd4491c8
Leandro da Silva Alves <leandro.alves@invillia.com>
2021-02-19 19:46:58 -0300

    ViewComponent de paginação

--- src/web/NSE.WebApp.MVC/Models/PagedViewModel.cs
+++ src/web/NSE.WebApp.MVC/Models/PagedViewModel.cs
@@ -3,7 +3,17 @@ using System.Collections.Generic;
 
 namespace NSE.WebApp.MVC.Models
 {
-    public class PagedViewModel<T> where T: class
+    public interface IPagedList
+    {
+        int PageIndex { get; set; }
+        int PageSize { get; set; }
+        string Query { get; set; }
+        string ReferenceAction { get; set; }
+        double TotalPages { get; }
+        int TotalResults { get; set; }
+    }
+
+    public class PagedViewModel<T> : IPagedList where T : class
     {
         public string ReferenceAction { get; set; }
         public IEnumerable<T> List { get; set; }
